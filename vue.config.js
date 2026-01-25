045eaa17b7f184ad6766285cffac092775ce66a1
Abdulrahman Elbatea <139005150+abdelrhman445@users.noreply.github.com>
2026-01-25 17:36:04 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
+  // تفعيل تحويل التبعيات لضمان التوافق مع المتصفحات القديمة
   transpileDependencies: true,
+
+  // ✅ التحديث الأخير: قفل التفتيش التلقائي (Linting) أثناء الحفظ والـ Build
+  // ده هيحل مشكلة الـ Error اللي ظهرتلك في Vercel ويخلي الـ Build ينجح فوراً
   lintOnSave: false
 });
