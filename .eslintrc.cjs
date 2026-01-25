8634fe776b4d5cc75988b49bc15fb817e7270964
Abdulrahman Elbatea <139005150+abdelrhman445@users.noreply.github.com>
2026-01-25 17:31:40 +0200

    Create .eslintrc.cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: { node: true },
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended"],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/multi-word-component-names': 'off'
+  }
+};
